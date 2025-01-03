8091720f5767f4d1472325b34725b004f52fba00
Oluumd <oluumd@gmail.com>
2025-01-03 12:37:07 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,6 +43,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put your session id", // ඔයාගෙ සෙසන් එක දාගන්න / Put Your Session_id Here !!! 💚
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&GFd3zYRQ#2h7ZXWD_iHuyeh0klWR2MNzIpol0mkbosbLNd2UWfgA", // ඔයාගෙ සෙසන් එක දාගන්න / Put Your Session_id Here !!! 💚
 MONGODB: process.env.MONGODB || "put your mongodb url", // ඔයාගෙ Mongodb Url එක දාගන්න / Put Your Mongodb Url Here !!! 💚
 };
